0507db89bcf25433b750e85d45c98abceedd6480
vuong <yeungon@gmail.com>
2024-11-11 16:01:29 +0700

    Fix routing static

--- internal/boot/static.go
+++ internal/boot/static.go
@@ -1,8 +1,6 @@
 package boot
 
 import (
-	"fmt"
-	"log"
 	"mime"
 	"net/http"
 	"os"
@@ -13,10 +11,8 @@ import (
 )
 
 func LoadStatic(r *chi.Mux) {
-	log.Println("Loading static files handler...")
 	workDir, _ := os.Getwd()
 	filesDir := http.Dir(filepath.Join(workDir, "static"))
-	fmt.Println("Static files are served from:", filepath.Join(workDir, "static"))
 	FileServer(r, "/static", filesDir)
 }
 
@@ -26,31 +22,14 @@ func FileServer(r *chi.Mux, path string, root http.FileSystem) {
 		panic("FileServer does not permit any URL parameters.")
 	}
 
-	// Add trailing slash if missing, ensuring correct handling of the directory
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-
 	// Serve static files at the given path
 	r.Get(path+"*", func(w http.ResponseWriter, r *http.Request) {
-		// Log the requested URL path to help with debugging
-		log.Printf("Request URL: %s", r.URL.Path)
-
 		ext := filepath.Ext(path)
 		contentType := mime.TypeByExtension(ext)
-
 		if contentType != "" {
 			w.Header().Set("Content-Type", contentType)
 		}
-
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-
-		// Use http.StripPrefix to serve files correctly
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-
-		log.Printf("Serving file: %s", r.URL.Path)
+		fs := http.FileServer(root)
 		fs.ServeHTTP(w, r)
 	})
 
